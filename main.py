d38082b9dbd0c90127805abeac8e6803d537735a
Andreas Hagen <andreas@hagenz.com>
2018-08-09 09:59:51 +0200

    Switched to dxf and implemented basic functionality

--- main.py
+++ main.py
@@ -1,96 +1,69 @@
 import re
-import warnings
 from argparse import ArgumentParser, ArgumentTypeError
-from dataclasses import dataclass
 
-import requests
-from urllib3.exceptions import InsecureRequestWarning
-
-
-warnings.filterwarnings('ignore', category=InsecureRequestWarning)
+from dxf import DXF, DXFBase
+from requests import HTTPError
 
 
-@dataclass
-class Unauthorized(Exception):
-    realm: str
-    service: str
-    scope: list
-
-
-@dataclass
-class DockerAPIv2:
-    username: str
-    password: str
-    address: str
-
-    def _request(self, uri, **kwargs):
-        def request(**kwargs_):
-            response_ = requests.get(f'https://{self.address}/v2/{uri}', verify=False, **kwargs, **kwargs_)
-
-            if response_.status_code == 401:
-                entries = response_.headers['www-authenticate'] \
-                    .replace('Bearer ', '', 1) \
-                    .split(',')
-                authentication_details = {entry['key']: entry['value'] for entry in
-                                          (re.match('(?P<key>.*?)="(?P<value>.*?)"', entry) for entry in entries)}
-
-                raise Unauthorized(
-                    realm=authentication_details['realm'],
-                    service=authentication_details['service'],
-                    scope=authentication_details['scope'].split(' ')
-                )
-
-            return response_
-
-        try:
-            return request()
-        except Unauthorized as exception:
-            if self.username is None or self.password is None:
-                raise
-
-            response = requests.get(exception.realm, auth=(self.username, self.password), verify=False, params={
-                "service": exception.service,
-                "scope": exception.scope
-            })
-            return request(headers={'Authorization': f'Bearer {response.json()["token"]}'})
-
-    def catalog(self):
-        response = self._request('_catalog')
-        return response.json()['repositories']
-
-    def tags(self, repository):
-        response = self._request(f'{repository}/tags/list')
-        return response.json()['tags']
-
-
+# TODO: Token support instead of username/password
 def address_with_credentials(string):
     match = re.match('(?:^(?P<username>.+?):(?P<password>.+?)@)?(?P<address>.+?)$', string)
 
-    if not match:
-        raise ArgumentTypeError(f'"{string}" is not a valid connection string')
+    if match:
+        if match['username'] and match['password']:
+            def auth(dxf, response_):
+                dxf.authenticate(username=match['username'], password=match['password'], response=response_)
+        elif match['username'] or match['password']:
+            raise ArgumentTypeError(f'"{string}" is not a valid connection string')
+        else:
+            auth = None
 
-    return DockerAPIv2(**match.groupdict())
+        return DXFBase(match['address'], auth=auth, tlsverify=False)  # TODO: Get tlsverify from arguments instead
+    else:
+        raise ArgumentTypeError(f'"{string}" is not a valid connection string')
 
 
 parser = ArgumentParser(description='Mirror docker repositories')
-parser.add_argument('--source', required=True, type=address_with_credentials)
-parser.add_argument('--destination', required=True, type=address_with_credentials)
+parser.add_argument('-f', '--from', required=True, type=address_with_credentials, dest='source')
+parser.add_argument('-t', '--to', required=True, type=address_with_credentials, dest='destination')
+args = parser.parse_args()
+source = args.source
+destination = args.destination
 
+# TODO: Delete missing source repositories
+for repository in source.list_repos():
+    source_repository = DXF.from_base(source, repository)
+    destination_repository = DXF.from_base(destination, repository)
 
-def all_repository_tags(registry):
-    return {f'{repository}:{tag}' for repository in registry.catalog() for tag in registry.tags(repository)}
+    for tag in source_repository.list_aliases():
+        manifest_string, response = source_repository.get_manifest_and_response(tag)
+        manifest = response.json()
+        source_layers = [manifest["config"], *manifest['layers']]
 
+        try:
+            _, response = destination_repository.get_manifest_and_response(tag)
+            manifest = response.json()
+            destination_layers = [manifest["config"], *manifest['layers']]
+        except HTTPError:
+            missing_layers = source_layers
+            print(f'Creating {repository}:{tag}')
+        else:
+            missing_layers = [source_layer for source_layer in source_layers
+                              if not any(source_layer['digest'] == destination_layer['digest']
+                                         for destination_layer in destination_layers)]
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-    source = args.source
-    destination = args.destination
+            if missing_layers:
+                print(f'Updating {repository}:{tag} with missing layers')
+            else:
+                print(f'{repository}:{tag} is already up to date')
 
-    source_tags = all_repository_tags(source)
-    destination_tags = all_repository_tags(destination)
+        if missing_layers:
+            for layer in missing_layers:
+                print(f'Mirroring layer {layer["digest"]}')
 
-    print(source_tags)
-    print(destination_tags)
+                destination_repository.push_blob(
+                    digest=layer['digest'],
+                    data=source_repository.pull_blob(layer['digest']))
 
-    # mirror(source, destination, source_tags)
-    # delete(destination, destination_tags - source_tags)
+            print(f'Updating manifest for {repository}:{tag}')
+            destination_repository.set_manifest(tag, manifest_string)
