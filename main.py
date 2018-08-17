35d2df8ac839a7122a8995a7e7ef404a0ec42051
Andreas Hagen <andreas@hagenz.com>
2018-08-17 10:16:33 +0200

    Fixed bug where external layers were attempted mirrored

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from dxf import DXF, DXFBase
 from requests import HTTPError
 
 
-# TODO: Token support instead of username/password
 def address_with_credentials(string):
     match = re.match('(?:^(?P<username>.+?):(?P<password>.+?)@)?(?P<address>.+?)$', string)
 
@@ -27,23 +26,25 @@ parser = ArgumentParser(description='Mirror docker repositories')
 parser.add_argument('-f', '--from', required=True, type=address_with_credentials, dest='source')
 parser.add_argument('-t', '--to', required=True, type=address_with_credentials, dest='destination')
 args = parser.parse_args()
-source = args.source
-destination = args.destination
 
-# TODO: Delete missing source repositories
-for repository in source.list_repos():
+source = args.source
+source_repositories = source.list_repos()
+destination = args.destination
+destination_repositories = destination.list_repos()
+
+for repository in source_repositories:
     source_repository = DXF.from_base(source, repository)
     destination_repository = DXF.from_base(destination, repository)
 
     for tag in source_repository.list_aliases():
         manifest_string, response = source_repository.get_manifest_and_response(tag)
         manifest = response.json()
-        source_layers = [manifest["config"], *manifest['layers']]
+        source_layers = [manifest['config'], *manifest['layers']]
 
         try:
             _, response = destination_repository.get_manifest_and_response(tag)
             manifest = response.json()
-            destination_layers = [manifest["config"], *manifest['layers']]
+            destination_layers = [manifest['config'], *manifest['layers']]
         except HTTPError:
             missing_layers = source_layers
             print(f'Creating {repository}:{tag}')
@@ -57,6 +58,8 @@ for repository in source.list_repos():
             else:
                 print(f'{repository}:{tag} is already up to date')
 
+        missing_layers = [layer for layer in missing_layers if 'urls' not in layer or not layer['urls']]
+
         if missing_layers:
             for layer in missing_layers:
                 print(f'Mirroring layer {layer["digest"]}')
@@ -67,3 +70,7 @@ for repository in source.list_repos():
 
             print(f'Updating manifest for {repository}:{tag}')
             destination_repository.set_manifest(tag, manifest_string)
+
+# TODO: Delete missing source repositories
+#for repository in destination_repositories:
+#    if repository in source_repositories:
